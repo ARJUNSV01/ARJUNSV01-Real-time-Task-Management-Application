eec592a600e59db3174a733ec70b58a72fc72259
Arjun S V <arjun.v@medlinkjobs.com>
2024-08-01 15:13:52 +0530

    Sequelize module configure and connected to pg

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,11 +1,20 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { TaskModule } from './task/task.module';
 
 @Module({
-  imports: [TaskModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: '12345678',
+      database: 'Task_Management',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    TaskModule,
+  ],
 })
 export class AppModule {}
